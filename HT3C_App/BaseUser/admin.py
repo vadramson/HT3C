4684d347ceaee2f4740f60edb5586334116f98f5
vadramson <vadramson@gmail.com>
2018-05-09 17:19:47 +0100

    All migrations go Successfully and basic changes to the Django-Admin Login interface and normal user login interface. Also Major changes in the admin of Administration Module for custom user Management

--- HT3C_App/BaseUser/admin.py
+++ HT3C_App/BaseUser/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 # Register your models here.
+from BaseUser.models import Student
+
+
+class StudentInline(admin.StackedInline):
+    model = Student
+    can_delete = False
+    verbose_name_plural = 'Student'
+    fk_name = 'user'
+
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (StudentInline,)
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'get_role', 'get_birth_date', 'get_departmet', 'get_birth_place')
+    list_select_related = ('student',)
+
+    def get_departmet(self, instance):
+        return instance.student.department
+
+    get_departmet.short_description = 'Student`s Dpt.'
+
+    def get_role(self, instance):
+        return instance.student.role
+
+    get_role.short_description = 'User Role'
+
+    def get_birth_date(self, instance):
+        return instance.student.birthDate
+
+    get_birth_date.short_description = 'Birth Date'
+
+    def get_birth_place(self, instance):
+        return instance.student.birthPlace
+
+    get_birth_place.short_description = 'Birth Place'
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
